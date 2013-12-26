3f9ee9cb288aa721618e2b0ebcd38a79012b1729
Zach Hall <zach@sosguy.net>
2013-12-26 00:12:53 -0500

    Initial commit. Some logic written, most graphics finished, working on presentation.

---
+++ js/game.js
@@ -0,0 +1,191 @@
+/*globals Kinetic, Image*/
+
+// PRESENTATION CODE
+Array.prototype.randomItem = function() {
+    return this[Math.floor(Math.random()*this.length)];
+};
+
+var stage = new Kinetic.Stage({
+    container: 'game',
+    width: 504,
+    height: 360
+});
+
+var suits = {
+    hearts: [{"x":37,"y":2,"width":33,"height":39}],
+    spades: [{"x":2,"y":2,"width":33,"height":39}],
+    diamonds: [{"x":72,"y":2,"width":33,"height":39}],
+    clubs: [{"x":107,"y":2,"width":33,"height":39}],
+    blank: [{"x":142,"y":2,"width":33,"height":39}]
+};
+
+var ranks = {
+    a: [{"x":353,"y":2,"width":42,"height":35}],
+    2: [{"x":705,"y":2,"width":42,"height":35}],
+    3: [{"x":661,"y":2,"width":42,"height":35}],
+    4: [{"x":617,"y":2,"width":42,"height":35}],
+    5: [{"x":573,"y":2,"width":42,"height":35}],
+    6: [{"x":529,"y":2,"width":42,"height":35}],
+    7: [{"x":485,"y":2,"width":42,"height":35}],
+    8: [{"x":441,"y":2,"width":42,"height":35}],
+    9: [{"x":397,"y":2,"width":42,"height":35}],
+    10: [{"x":749,"y":2,"width":42,"height":35}],
+    j: [{"x":265,"y":2,"width":42,"height":35}],
+    q: [{"x":221,"y":2,"width":42,"height":35}],
+    k: [{"x":177,"y":2,"width":42,"height":35}],
+    blank: [{"x":309,"y":2,"width":42,"height":35}]
+};
+
+var system = {
+    0: [{"x":2,"y":67,"width":23,"height":22}],
+    1: [{"x":77,"y":43,"width":23,"height":22}],
+    2: [{"x":127,"y":43,"width":23,"height":22}],
+    3: [{"x":2,"y":43,"width":23,"height":22}],
+    4: [{"x":27,"y":43,"width":23,"height":22}],
+    5: [{"x":202,"y":43,"width":23,"height":22}],
+    6: [{"x":227,"y":43,"width":23,"height":22}],
+    7: [{"x":52,"y":43,"width":23,"height":22}],
+    8: [{"x":152,"y":43,"width":23,"height":22}],
+    9: [{"x":177,"y":43,"width":23,"height":22}],
+    gameover_off: [{"x":2,"y":32,"width":123,"height":9}],
+    gameover_on: [{"x":127,"y":32,"width":123,"height":9}, {"x":2,"y":32,"width":123,"height":9}],
+    draw_off: [{"x":2,"y":2,"width":122,"height":8}],
+    draw_on: [{"x":126,"y":2,"width":122,"height":8}, {"x":2,"y":2,"width":122,"height":8}],
+    overflow_off: [{"x":2,"y":12,"width":145,"height":8}],
+    overflow_on: [{"x":2,"y":22,"width":145,"height":8}],
+    win_off: [{"x":27,"y":67,"width":77,"height":26}],
+    win_on: [{"x":106,"y":67,"width":77,"height":26}, {"x":27,"y":67,"width":77,"height":26}],
+    awe_off: [{"x":185,"y":67,"width":32,"height":32}],
+    awe_on: [{"x":219,"y":67,"width":32,"height":32}, {"x":185,"y":67,"width":32,"height":32}],
+    held_off: [{"x":149,"y":12,"width":44,"height":8}],
+    held_on: [{"x":195,"y":12,"width":44,"height":8}],
+    blank: [{"x":102,"y":43,"width":23,"height":22}]
+};
+
+var cardLayer = new Kinetic.Layer();
+var scoreLayer = new Kinetic.Layer();
+var cardsImg = new Image();
+var systemImg = new Image();
+var cards = {};
+var scoreboard = {};
+
+function CardView (index) {
+    cards[index] = {};
+    var spacing = 84;
+    
+    cards[index].suit = new Kinetic.Sprite({
+        x: 45+index*spacing, // 45
+        y: 185, // 185
+        image: cardsImg,
+        animation: 'blank',
+        animations: suits,
+        frameRate: 1,
+        index: 0
+    });
+    
+    cards[index].rank = new Kinetic.Sprite({
+        x: 80+index*spacing, // 80
+        y: 190, // 190
+        image: cardsImg,
+        animation: 'blank',
+        animations: ranks,
+        frameRate: 1,
+        index: 0
+    });
+    
+    cards[index].held = new Kinetic.Sprite({
+        x: 78+index*spacing,
+        y: 180,
+        image: systemImg,
+        animation: 'held_off',
+        animations: system,
+        frameRate: 1,
+        index: 0
+    });
+    
+    cardLayer.add(cards[index].suit);
+    cardLayer.add(cards[index].rank);
+    cardLayer.add(cards[index].held);
+}
+
+cardsImg.onload = function() {
+    // initialize 5 cards
+    for (var i = 0; i < 5; i++) {
+        CardView(i);
+        cards[i].rank.setAnimation(['a', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q', 'k'].randomItem());
+        //cards[i].suit.setAnimation(['spades', 'hearts', 'clubs', 'diamonds'].randomItem());
+        //cards[i].held.setAnimation(['held_off', 'held_on'].randomItem());
+    }
+   
+    stage.add(cardLayer);
+    cards[0].rank.setAnimation(3);
+    
+    var deck = new PokerDeck(true);
+    var hand = deck.dealHand();
+//print(deck.dealHand());
+
+    for (var i = 0; i < hand.cards.length; i++) {
+        cards[i].rank.setAnimation(hand.cards[i].rank);
+        cards[i].suit.setAnimation(hand.cards[i].suit);
+    }
+};
+
+systemImg.onload = function() {
+    // initialize scoreboard
+    for (var i = 0; i < 5; i++) {
+        scoreboard[i] = new Kinetic.Sprite({
+            x: 315+i*30,
+            y: 140,
+            image: systemImg,
+            animation: 'blank',
+            animations: system
+        });
+        //scoreboard[i].setAnimation(i);
+        scoreLayer.add(scoreboard[i]);
+    }
+    
+    scoreboard.overflow = new Kinetic.Sprite({
+        x: 313, y: 165, animation: 'overflow_off',
+        image: systemImg, animations: system,
+    });
+    scoreLayer.add(scoreboard.overflow);
+    
+    scoreboard.holdOrDraw = new Kinetic.Sprite({
+        x: 50, y: 140, frameRate: 2, animation: 'draw_off',
+        image: systemImg, animations: system,
+    });
+    scoreLayer.add(scoreboard.holdOrDraw);
+    scoreboard.holdOrDraw.start();
+    
+    scoreboard.gameover = new Kinetic.Sprite({
+        x: 50, y: 160, frameRate: 1, animation: 'gameover_on',
+        image: systemImg, animations: system,
+    });
+    scoreLayer.add(scoreboard.gameover);
+    scoreboard.gameover.start();
+    
+    scoreboard.win = new Kinetic.Sprite({
+        x: 180, y: 140, frameRate: 3, animation: 'win_off',
+        image: systemImg, animations: system,
+    });
+    scoreLayer.add(scoreboard.win);
+    scoreboard.win.start();
+    
+    scoreboard.awe = new Kinetic.Sprite({
+        x: 270, y: 137, frameRate: 1, animation: 'awe_off',
+        image: systemImg, animations: system,
+    });
+    scoreLayer.add(scoreboard.awe);
+    scoreboard.awe.start();
+    
+    stage.add(scoreLayer);
+    
+    // initialize other elements
+    cardsImg.src = 'img/cards.png';
+};
+
+systemImg.src = 'img/system.png';
+
+function print (message) {
+    document.getElementById('messages').innerHTML += message + "<br/>";
+}
---
+++ js/logic.coffee
@@ -0,0 +1,164 @@
+Array.prototype.shuffle = ->
+    counter = this.length
+    temp = undefined
+    index = undefined
+  
+    # While there are elements in the array
+    while counter > 0
+        # Pick a random index
+        index = Math.floor(Math.random() * counter)
+        
+        # Decrease counter by 1
+        counter--
+        
+        # And swap the last element with it
+        temp =this[counter]
+        this[counter] = this[index]
+        this[index] = temp
+
+d_suits =
+    0:'Spades',
+    1:'Clubs',
+    2:'Hearts',
+    3:'Diamonds'
+
+d_ranks =
+    2:'2',
+    3:'3',
+    4:'4',
+    5:'5',
+    6:'6',
+    7:'7',
+    8:'8',
+    9:'9',
+    10:'10',
+    11:'Jack',
+    12:'Queen',
+    13:'King',
+    14:'Ace'
+
+d_bet_multiplier =
+    0:0,
+    1:0.5,
+    2:3,
+    3:7,
+    4:15,
+    5:30,
+    6:100,
+    7:150,
+    8:400,
+    9:5000
+
+class Card
+    constructor: (suit, rank) ->
+        @held = false
+        @suit = suit
+        @rank = rank
+    toString: -> "#{@held}\t#{d_ranks[@rank]} of #{d_suits[@suit]}"
+
+class Deck
+    constructor: (shuffle) ->
+        @cards = []
+        for suit in [0..3]
+            for rank in [2..14]
+                @cards.push new Card(suit, rank)
+        if shuffle then @shuffle()
+    toString: ->
+        o = ''
+        for card in @cards
+            o += "#{card}\t\n"
+        o
+    shuffle: -> @cards.shuffle()
+    popCard: -> @cards.pop()
+
+class PokerDeck extends Deck
+    dealHand: ->
+        hand = new PokerHand(this)
+        for i in [0..4]
+            card = @popCard()
+            hand.cards.push card
+        hand
+
+class PokerHand extends Deck
+    constructor: (parent, cards=[]) ->
+        @parent = parent
+        @cards = []
+        for card in cards
+            @cards.push card
+        @getCurrentRanks()
+        @getCurrentSuits()
+    
+    toggleHold: (cardIndex) ->
+        if @cards[cardIndex].held then @cards[cardIndex].held = false
+        else @cards[cardIndex].held = true
+    
+    draw: ->
+        for i in [0..@cards.length-1]
+            card = @cards[i]
+            if !card.held
+                @parent.cards.splice 0, 0, @cards.splice(i, 1)
+                @cards.splice 1, 0, @parent.popCard()
+    
+    determinePokerHand: ->
+        @getCurrentRanks()
+        @getCurrentSuits()
+        if @royalFlush() then 9
+        else if @straightFlush() then 8
+        else if @fourKind() then 7
+        else if @fullHouse() then 6
+        else if @flush() then 5
+        else if @straight() then 4
+        else if @threeKind() then 3
+        else if @twoPair() then 2
+        else if @jacksOrBetter() then 1
+        else 0
+    
+    getCurrentSuits: ->
+        @suits = {}
+        for card in @cards
+            @suits[card.suit] = if !@suits[card.suit] then 1 else @suits[card.suit]++
+    
+    getCurrentRanks: ->
+        @ranks = {}
+        for card in @cards
+            @ranks[card.rank] = if !@ranks[card.rank] then 1 else @ranks[card.rank]++
+    
+    sameRank: (n) ->
+        for rank in @ranks
+            if rank == n then true
+        false
+    
+    royalFlush: ->
+        if @flush()
+            for rank in [10..14]
+                if @ranks[rank] != 1 then false
+            true
+        false
+    
+    straightFlush: -> @straight() and @flush()
+    fourKind: -> @sameRank 4
+    threeKind: -> @sameRank 3
+    pair: -> @sameRank 2
+    fullHouse: -> @pair() and @threeKind()
+    
+    flush: ->
+        for suit in @suits
+            if suit == 5 then true
+        false
+    
+    straight: ->
+        cardsInRow = 0
+        startingRank = 0
+        for rank in [2..14]
+            if @ranks[rank] == 1
+                startingRank = rank
+                break
+        for rank in [startingRank..startingRank+5]
+            if @ranks[rank] == 1 then cardsInRow++
+        if cardsInRow == 5 then true else false
+    
+    twoPair: ->
+        pairs = 0
+        for rank in @ranks
+            if rank == 2 then pairs++
+        if pairs == 2 then true else false
